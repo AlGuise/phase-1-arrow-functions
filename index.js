7fb3847fb54efae642a43554cac8acbfcc4ba44b
Alain Giessel <alain.giessel@gmail.com>
2022-02-23 20:32:40 -0700

    commit tree a2a98fef1d0d2f5600801c3d7be54fe2aa3606a9

---
+++ index.js
@@ -0,0 +1,10 @@
+// const divide = (num1 , num2) => {
+//     console.log(`Dividing ${num1}`);
+//     console.log(`Dividing ${num2}`);
+//     return num1/num2
+// }
+// divide(2000, 100);
+
+const divide = math => 2000/100
+const square = x => x * x; 
+const add = (x, y) => x + y
